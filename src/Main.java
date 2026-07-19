91e5fb83a97f07718cf8dad6bc4d9c0b36977366
ehohlov <lilkamag73@yandex.ru>
2026-07-19 15:46:59 +0300

    Fourth

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog - 3.5);
-        System.out.println(cat - 1.6);
-        System.out.println(paper - 7639);
+        var friend = 19;
+        System.out.println(friend);
+        System.out.println(friend + 2);
+        System.out.println((friend + 2)/7);
     }
 }
