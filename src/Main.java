56c9312d62eba4e59af22569b33769c5ed7f84c0
ehohlov <lilkamag73@yandex.ru>
2026-07-19 16:06:44 +0300

    Six

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println(frog);
-        System.out.println(frog * 10);
-        System.out.println((frog * 10)/3.5);
-        System.out.println((frog * 10)/3.5);
-        System.out.println(((frog * 10)/3.5)+4);
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7 ;
+        var totalWeight = firstBoxer + secondBoxer ;
+        System.out.println("Общая масса: " + totalWeight + " кг");
+        var weightDifference = firstBoxer - secondBoxer ;
+        if (weightDifference < 0) {
+            weightDifference = -weightDifference;
+        }
+        System.out.println("Разница масс: " + weightDifference + " кг");
     }
 }
