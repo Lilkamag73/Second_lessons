0d5adda7edcc5701fc92ab1bdd3fa9ba3f0f9892
ehohlov <lilkamag73@yandex.ru>
2026-07-19 16:32:20 +0300

    8.2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var time = 640;
         var timePerChar = 8;
-        var result = time / timePerChar;
-        System.out.println("Всего в компнаии работает - " + result + " человек");
+        var people = time / timePerChar;
+        var maxPeople = people + 94;
+        var maxTime = maxPeople * timePerChar;
+        System.out.println("Если в комании будет работаь " + maxPeople + " человек, то всего " + maxTime + " часов работы может быть поделено между сотрудрниками");
     }
 }
