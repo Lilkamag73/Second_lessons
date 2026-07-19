0b51bdec6adbab80297e1c29a4c8cb4ee9dddf32
ehohlov <lilkamag73@yandex.ru>
2026-07-19 15:37:54 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println(dog + 4);
+        System.out.println(cat + 4);
+        System.out.println(paper + 4);
     }
 }
