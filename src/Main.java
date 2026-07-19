aeec3cae4ed18384efa2abc9fcc68329fde57aa2
ehohlov <lilkamag73@yandex.ru>
2026-07-19 16:09:27 +0300

    Seven

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ public class Main {
     public static void main(String[] args) {
         var firstBoxer = 78.2;
         var secondBoxer = 82.7 ;
-        var totalWeight = firstBoxer + secondBoxer ;
-        System.out.println("Общая масса: " + totalWeight + " кг");
-        var weightDifference = firstBoxer - secondBoxer ;
-        if (weightDifference < 0) {
-            weightDifference = -weightDifference;
-        }
-        System.out.println("Разница масс: " + weightDifference + " кг");
+        var diffWeight = secondBoxer % firstBoxer;
+        System.out.println(diffWeight);
     }
 }
